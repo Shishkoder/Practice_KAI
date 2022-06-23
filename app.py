74fb6ef72c2e1f00106911eb0edc52c4be17355f
ef@gmail.com <94549535+S1GHE@users.noreply.github.com>
2022-06-23 13:58:32 +0300

    edd

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world(): 
-    return 'Hello World!'
+def main_page(): 
+    return render_template('index.html', title="Главная страница")
 
 
 if __name__ == '__main__':
